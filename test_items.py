13d22607578bf856a90adf2e33b7c70a734ddfba
Nastasja <33484571+zastasja@users.noreply.github.com>
2023-07-18 10:30:04 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -7,7 +7,7 @@ all_products = (By.XPATH, '//*[@id="browse"]/li/ul/li[1]/a')
 book_item = (By.XPATH, '//img[@alt="Coders at Work"]')
 add_to_backet_btn = (By.XPATH, '//*[@id="add_to_basket_form"]/button')
 
-def test_add_product_btn_available_in_french(browser):
+def test_add_product_btn_available(browser):
     browser.get(main_url)
     browser.find_element(*all_products).click()
     browser.find_element(*book_item).click()
